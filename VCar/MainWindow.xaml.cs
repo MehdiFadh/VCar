19957006d012842400a855ee50edc5673908eedd
Mehdi <mfadhloune@gmail.com>
2024-09-06 23:11:19 +0200

    Ajout de la base de donnée et de touts les messagesbox pour les erreurs et autres

--- VCar/MainWindow.xaml.cs
+++ VCar/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace VCar
 {
@@ -30,11 +31,33 @@ namespace VCar
 
         private void butConnexion_Click(object sender, RoutedEventArgs e)
         {
-            if(txtIdentifiant.Text == "mehdi" && txtPassword.Password == "password")
+            if(txtIdentifiant.Text == "mehdi" && txtPassword.Password == "toothless")
             {
-                gridConnexion.Visibility = Visibility.Collapsed;
-                this.Title = "Vcar - Accueil";
+                NpgsqlConnection connexion = new NpgsqlConnection();
+                connexion.ConnectionString = "Server=ep-morning-wood-a27nq1on.eu-central-1.aws.neon.tech;" +
+                                            "port=5432;" +
+                                            "Database=eligiusdb;" +
+                                            "uid=eligiusdb_owner;" +
+                                            "password=tf4Ag8qwiJoT;";
+
+                try
+                {
+                    connexion.Open();
+                    MessageBox.Show("Connecté","Connexion",MessageBoxButton.OK,MessageBoxImage.Information);
+                    gridConnexion.Visibility = Visibility.Collapsed;
+                    window.Title = "Vcar - Accueil";
+                }
+                catch
+                {
+                    MessageBox.Show("Erreur de connexion à la base de données", "Connexion", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
+            else
+            {
+                MessageBox.Show("L'identifiant ou le mot de passe est incorrect", "Connexion", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            
         }
     }
 }
